91e8c366bc507c9fdae9057da062505147081659
Diivvuu <divyanshubawa9@gmail.com>
2024-05-14 01:44:21 +0530

    completed

--- learning/index.js
+++ learning/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const fs = require("node:fs");
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -12,6 +12,27 @@ app.get("/", function (req, res) {
   fs.readdir(`./files`, function (err, files) {
     res.render("index", { files: files });
   });
+});
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", {
+        name: req.params.filename,
+        data: filedata,
+      });
+    }
+  );
+});
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.task,
+    function (err) {
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(3000);
