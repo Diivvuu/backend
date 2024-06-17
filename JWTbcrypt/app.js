705c267b993be03adac9a6131f206734ea39942d
Diivvuu <divyanshubawa9@gmail.com>
2024-06-18 02:55:46 +0530

    bcrypt salt and hash done

--- JWTbcrypt/app.js
+++ JWTbcrypt/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const userModel = require("./models/user");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const cookieParser = require("cookie-parser");
@@ -12,5 +13,20 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.post("/create", async (req, res) => {
+  let { username, email, password, age } = req.body;
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      // Store hash in your password DB.
+      let newUser = await userModel.create({
+        username,
+        email,
+        password: hash,
+        age,
+      });
+      res.send(newUser);
+    });
+  });
 });
 app.listen(3000);
