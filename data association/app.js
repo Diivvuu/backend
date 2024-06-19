f478ae9be08027bc56814a4ffa03ca51fd2d9a1f
Diivvuu <divyanshubawa9@gmail.com>
2024-06-20 04:57:06 +0530

    signup done , doing signin

--- data association/app.js
+++ data association/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-const userModel = require("./models/users");
+const userModel = require("./models/user");
+const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -11,4 +14,51 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.listen(3000);
+app.get("/signin", (req, res) => {
+  res.render("Login");
+});
+app.post("/signup", async (req, res) => {
+  const { Username, Name, Email, Password, Age } = req.body;
+  let user = await userModel.findOne({ email: Email });
+
+  if (user) return res.status(400).render("goToLogin");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(Password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        username: Username,
+        name: Name,
+        email: Email,
+        password: hash,
+        age: Age,
+      });
+      let token = jwt.sign({ email: Email, userid: user._id }, "shhh");
+      res.cookie("token", token);
+      res.render("registered successfully");
+    });
+  });
+});
+app.post("/signin", async (req, res) => {
+  const { Username, Name, Email, Password, Age } = req.body;
+  let user = await userModel.findOne({ email: Email });
+
+  if (user) return res.status(400).send("User already registered");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(Password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        username: Username,
+        name: Name,
+        email: Email,
+        password: hash,
+        age: Age,
+      });
+      let token = jwt.sign({ email: Email, userid: user._id }, "shhh");
+      res.cookie("token", token);
+      res.send("registered successfully");
+    });
+  });
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
--- data association/models/user.js
+++ data association/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:270177/miniproject");
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
 
 const userSchema = mongoose.Schema({
   username: String,
@@ -7,6 +7,12 @@ const userSchema = mongoose.Schema({
   age: Number,
   email: String,
   password: String,
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
 });
 
 module.exports = mongoose.model("user", userSchema);
